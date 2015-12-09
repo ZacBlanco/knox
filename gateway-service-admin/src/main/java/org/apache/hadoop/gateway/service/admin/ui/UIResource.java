64e921adacb3b272675c077465ae1ee2c45043a1
zblanco <zblanco@apache.org>
2015-12-09 18:19:27 -0500

    init for new UI resource

---
+++ gateway-service-admin/src/main/java/org/apache/hadoop/gateway/service/admin/ui/UIResource.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.gateway.service.admin.ui;
+
+import org.eclipse.persistence.oxm.MediaType;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static javax.ws.rs.core.MediaType.TEXT_HTML;
+
+@Path("/ui")
+public class UIResource {
+  @Context
+  private HttpServletRequest request;
+
+  @GET
+  @Produces({TEXT_HTML})
+  @Path("home")
+  public InputStream getHome() throws FileNotFoundException{
+    InputStream i = this.getClass().getResourceAsStream("index.html");
+    return i;
+  }
+
+
+
+
+}
+
---
+++ gateway-test/src/test/java/org/apache/hadoop/gateway/GatewayAdminUITest.java
@@ -0,0 +1,445 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.gateway;
+
+import com.jayway.restassured.http.ContentType;
+import com.mycila.xmltool.XMLDoc;
+import com.mycila.xmltool.XMLTag;
+import org.apache.directory.server.protocol.shared.transport.TcpTransport;
+import org.apache.hadoop.gateway.config.GatewayConfig;
+import org.apache.hadoop.gateway.security.ldap.SimpleLdapDirectoryServer;
+import org.apache.hadoop.gateway.services.DefaultGatewayServices;
+import org.apache.hadoop.gateway.services.GatewayServices;
+import org.apache.hadoop.gateway.services.ServiceLifecycleException;
+import org.apache.hadoop.gateway.services.topology.TopologyService;
+import org.apache.hadoop.gateway.topology.Param;
+import org.apache.hadoop.gateway.topology.Provider;
+import org.apache.hadoop.gateway.topology.Service;
+import org.apache.hadoop.gateway.topology.Topology;
+import org.apache.hadoop.test.TestUtils;
+import org.apache.http.HttpStatus;
+import org.apache.log4j.Appender;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.apache.hadoop.test.TestUtils.LOG_ENTER;
+import static org.apache.hadoop.test.TestUtils.LOG_EXIT;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class GatewayAdminUITest {
+
+  private static final long SHORT_TIMEOUT = 1000L;
+  private static final long MEDIUM_TIMEOUT = 5 * SHORT_TIMEOUT;
+  private static final long LONG_TIMEOUT = 5 * MEDIUM_TIMEOUT;
+
+  private static Class RESOURCE_BASE_CLASS = GatewayAdminTopologyFuncTest.class;
+  private static Logger LOG = LoggerFactory.getLogger( GatewayAdminTopologyFuncTest.class );
+
+  public static Enumeration<Appender> appenders;
+  public static GatewayConfig config;
+  public static GatewayServer gateway;
+  public static String gatewayUrl;
+  public static String clusterUrl;
+  public static SimpleLdapDirectoryServer ldap;
+  public static TcpTransport ldapTransport;
+
+  @BeforeClass
+  public static void setupSuite() throws Exception {
+    //appenders = NoOpAppender.setUp();
+    setupLdap();
+    setupGateway(new GatewayTestConfig());
+  }
+
+  @AfterClass
+  public static void cleanupSuite() throws Exception {
+    gateway.stop();
+    ldap.stop( true );
+    //FileUtils.deleteQuietly( new File( config.getGatewayHomeDir() ) );
+    //NoOpAppender.tearDown( appenders );
+  }
+
+  public static void setupLdap() throws Exception {
+    URL usersUrl = getResourceUrl( "users.ldif" );
+    int port = findFreePort();
+    ldapTransport = new TcpTransport( port );
+    ldap = new SimpleLdapDirectoryServer( "dc=hadoop,dc=apache,dc=org", new File( usersUrl.toURI() ), ldapTransport );
+    ldap.start();
+    LOG.info( "LDAP port = " + ldapTransport.getPort() );
+  }
+
+  public static void setupGateway(GatewayTestConfig testConfig) throws Exception {
+
+    File targetDir = new File( System.getProperty( "user.dir" ), "target" );
+    File gatewayDir = new File( targetDir, "gateway-home-" + UUID.randomUUID() );
+    gatewayDir.mkdirs();
+
+    config = testConfig;
+    testConfig.setGatewayHomeDir( gatewayDir.getAbsolutePath() );
+
+    File topoDir = new File( testConfig.getGatewayTopologyDir() );
+    topoDir.mkdirs();
+
+    File deployDir = new File( testConfig.getGatewayDeploymentDir() );
+    deployDir.mkdirs();
+
+    File descriptor = new File( topoDir, "admin.xml" );
+    FileOutputStream stream = new FileOutputStream( descriptor );
+    createKnoxTopology().toStream( stream );
+    stream.close();
+
+    File descriptor2 = new File( topoDir, "test-cluster.xml" );
+    FileOutputStream stream2 = new FileOutputStream( descriptor2 );
+    createNormalTopology().toStream( stream2 );
+    stream.close();
+
+
+    DefaultGatewayServices srvcs = new DefaultGatewayServices();
+    Map<String,String> options = new HashMap<String,String>();
+    options.put( "persist-master", "false" );
+    options.put( "master", "password" );
+
+    try {
+      srvcs.init( testConfig, options );
+    } catch ( ServiceLifecycleException e ) {
+      e.printStackTrace(); // I18N not required.
+    }
+    gateway = GatewayServer.startGateway( testConfig, srvcs );
+    MatcherAssert.assertThat( "Failed to start gateway.", gateway, notNullValue() );
+
+    LOG.info( "Gateway port = " + gateway.getAddresses()[ 0 ].getPort() );
+
+    gatewayUrl = "http://localhost:" + gateway.getAddresses()[0].getPort() + "/" + config.getGatewayPath();
+    clusterUrl = gatewayUrl + "/admin";
+  }
+
+  private static XMLTag createNormalTopology() {
+    XMLTag xml = XMLDoc.newDocument( true )
+        .addRoot( "topology" )
+        .addTag( "gateway" )
+        .addTag( "provider" )
+        .addTag( "role" ).addText( "webappsec" )
+        .addTag( "name" ).addText( "WebAppSec" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "param" )
+        .addTag( "name" ).addText( "csrf.enabled" )
+        .addTag( "value" ).addText( "true" ).gotoParent().gotoParent()
+        .addTag( "provider" )
+        .addTag( "role" ).addText( "authentication" )
+        .addTag( "name" ).addText( "ShiroProvider" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm" )
+        .addTag( "value" ).addText( "org.apache.hadoop.gateway.shirorealm.KnoxLdapRealm" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.userDnTemplate" )
+        .addTag( "value" ).addText( "uid={0},ou=people,dc=hadoop,dc=apache,dc=org" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.contextFactory.url" )
+        .addTag( "value" ).addText( "ldap://localhost:" + ldapTransport.getPort() ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.contextFactory.authenticationMechanism" )
+        .addTag( "value" ).addText( "simple" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "urls./**" )
+        .addTag( "value" ).addText( "authcBasic" ).gotoParent().gotoParent()
+        .addTag( "provider" )
+        .addTag( "role" ).addText( "identity-assertion" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "name" ).addText( "Default" ).gotoParent()
+        .addTag( "provider" )
+        .addTag( "role" ).addText( "authorization" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "name" ).addText( "AclsAuthz" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "webhdfs-acl" )
+        .addTag( "value" ).addText( "hdfs;*;*" ).gotoParent()
+        .gotoRoot()
+        .addTag( "service" )
+        .addTag( "role" ).addText( "WEBHDFS" )
+        .addTag( "url" ).addText( "http://localhost:50070/webhdfs/v1" ).gotoParent()
+        .gotoRoot();
+//     System.out.println( "GATEWAY=" + xml.toString() );
+    return xml;
+  }
+
+  private static XMLTag createKnoxTopology() {
+    XMLTag xml = XMLDoc.newDocument( true )
+        .addRoot( "topology" )
+        .addTag( "gateway" )
+        .addTag( "provider" )
+        .addTag( "role" ).addText( "authentication" )
+        .addTag( "name" ).addText( "ShiroProvider" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm" )
+        .addTag( "value" ).addText( "org.apache.hadoop.gateway.shirorealm.KnoxLdapRealm" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.userDnTemplate" )
+        .addTag( "value" ).addText( "uid={0},ou=people,dc=hadoop,dc=apache,dc=org" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.contextFactory.url" )
+        .addTag( "value" ).addText( "ldap://localhost:" + ldapTransport.getPort() ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "main.ldapRealm.contextFactory.authenticationMechanism" )
+        .addTag( "value" ).addText( "simple" ).gotoParent()
+        .addTag( "param" )
+        .addTag( "name" ).addText( "urls./**" )
+        .addTag( "value" ).addText( "authcBasic" ).gotoParent().gotoParent()
+        .addTag("provider")
+        .addTag( "role" ).addText( "authorization" )
+        .addTag( "name" ).addText( "AclsAuthz" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag("param")
+        .addTag("name").addText("knox.acl")
+        .addTag("value").addText("admin;*;*").gotoParent().gotoParent()
+        .addTag("provider")
+        .addTag( "role" ).addText( "identity-assertion" )
+        .addTag( "enabled" ).addText( "true" )
+        .addTag( "name" ).addText( "Default" ).gotoParent()
+        .addTag( "provider" )
+        .gotoRoot()
+        .addTag( "service" )
+        .addTag( "role" ).addText( "KNOX" )
+        .gotoRoot();
+    // System.out.println( "GATEWAY=" + xml.toString() );
+    return xml;
+  }
+
+  private static int findFreePort() throws IOException {
+    ServerSocket socket = new ServerSocket(0);
+    int port = socket.getLocalPort();
+    socket.close();
+    return port;
+  }
+
+  public static InputStream getResourceStream( String resource ) throws IOException {
+    return getResourceUrl( resource ).openStream();
+  }
+
+  public static URL getResourceUrl( String resource ) {
+    URL url = ClassLoader.getSystemResource( getResourceName( resource ) );
+    assertThat( "Failed to find test resource " + resource, url, Matchers.notNullValue() );
+    return url;
+  }
+
+  public static String getResourceName( String resource ) {
+    return getResourceBaseName() + resource;
+  }
+
+  public static String getResourceBaseName() {
+    return RESOURCE_BASE_CLASS.getName().replaceAll( "\\.", "/" ) + "/";
+  }
+
+  //@Test
+  public void waitForManualTesting() throws IOException {
+    System.in.read();
+  }
+
+  @Test( timeout = LONG_TIMEOUT )
+  public void testTopologyCollection() throws ClassNotFoundException {
+    LOG_ENTER();
+
+    String username = "admin";
+    String password = "admin-password";
+    String serviceUrl =  clusterUrl + "/api/v1/topologies";
+    String href1 = given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .body("topologies.topology[0].name", not(nullValue()))
+        .body("topologies.topology[1].name", not(nullValue()))
+        .body("topologies.topology[0].uri", not(nullValue()))
+        .body("topologies.topology[1].uri", not(nullValue()))
+        .body("topologies.topology[0].href", not(nullValue()))
+        .body("topologies.topology[1].href", not(nullValue()))
+        .body("topologies.topology[0].timestamp", not(nullValue()))
+        .body("topologies.topology[1].timestamp", not(nullValue()))
+        .when().get(serviceUrl).thenReturn().getBody().path("topologies.topology.href[1]");
+
+    given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_XML)
+        .expect()
+            //.log().all()
+        .body("topologies.topology.href[1]", equalTo(href1))
+        .statusCode(HttpStatus.SC_OK)
+        .when().get(serviceUrl);
+
+
+    given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .contentType(MediaType.APPLICATION_XML)
+        .get(serviceUrl);
+
+
+    given().auth().preemptive().basic(username, password)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .contentType("application/json")
+        .body("topology.name", equalTo("test-cluster"))
+        .when().get(href1);
+
+    LOG_EXIT();
+  }
+
+  @Test( timeout = LONG_TIMEOUT )
+  public void testTopologyObject() throws ClassNotFoundException {
+    LOG_ENTER();
+
+    String username = "admin";
+    String password = "admin-password";
+    String serviceUrl =  clusterUrl + "/api/v1/topologies";
+    String hrefJson = given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_JSON)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .when().get(serviceUrl).thenReturn().getBody().path("topologies.topology[1].href");
+
+    String timestampJson = given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_JSON)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .contentType("application/json")
+        .when().get(serviceUrl).andReturn()
+        .getBody().path("topologies.topology[1].timestamp");
+
+    given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_JSON)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .body("topology.name", equalTo("test-cluster"))
+        .body("topology.timestamp", equalTo(Long.parseLong(timestampJson)))
+        .when()
+        .get(hrefJson);
+
+
+    String hrefXml = given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_XML)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .when().get(serviceUrl).thenReturn().getBody().path("topologies.topology[1].href");
+
+    given()
+        //.log().all()
+        .auth().preemptive().basic(username, password)
+        .header("Accept", MediaType.APPLICATION_XML)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .when()
+        .get(hrefXml);
+
+    LOG_EXIT();
+  }
+
+  @Test( timeout = LONG_TIMEOUT )
+  public void testPositiveAuthorization() throws ClassNotFoundException {
+    LOG_ENTER();
+
+    String adminUser = "admin";
+    String adminPass = "admin-password";
+    String url = clusterUrl + "/api/v1/topologies";
+
+    given()
+        //.log().all()
+        .auth().preemptive().basic(adminUser, adminPass)
+        .expect()
+            //.log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .contentType(ContentType.JSON)
+        .body("topologies.topology[0].name", not(nullValue()))
+        .body("topologies.topology[1].name", not(nullValue()))
+        .body("topologies.topology[0].uri", not(nullValue()))
+        .body("topologies.topology[1].uri", not(nullValue()))
+        .body("topologies.topology[0].href", not(nullValue()))
+        .body("topologies.topology[1].href", not(nullValue()))
+        .body("topologies.topology[0].timestamp", not(nullValue()))
+        .body("topologies.topology[1].timestamp", not(nullValue()))
+        .get(url);
+
+    LOG_EXIT();
+  }
+
+  @Test
+  public void testAdminUI() throws ClassNotFoundException {
+    TestUtils.LOG_ENTER();
+
+    String username = "admin";
+    String password = "admin-password";
+    String serviceUrl =  clusterUrl + "/ui/home";
+    given()
+//        .log().all()
+        .auth().preemptive().basic( username, password )
+        .expect()
+//        .log().all()
+        .statusCode(HttpStatus.SC_OK)
+        .when().get( serviceUrl );
+
+    TestUtils.LOG_EXIT();
+  }
+}
